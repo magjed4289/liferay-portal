adde4549f7236d660df677930866153a08fa78af
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2024-09-19 21:43:45 +0200

    LPD-35336 Test objects

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/OpenAPIResourceTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/OpenAPIResourceTest.java
@@ -18,13 +18,19 @@ import com.liferay.object.field.builder.MultiselectPicklistObjectFieldBuilder;
 import com.liferay.object.field.util.ObjectFieldUtil;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectField;
+import com.liferay.object.rest.dto.v1_0.ObjectEntry;
+import com.liferay.object.rest.internal.odata.entity.v1_0.test.ObjectEntryEntityModelTest;
+import com.liferay.object.rest.resource.v1_0.ObjectEntryResource;
 import com.liferay.object.service.ObjectActionLocalService;
 import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectDefinitionLocalServiceUtil;
 import com.liferay.object.service.ObjectRelationshipLocalServiceUtil;
 import com.liferay.object.system.JaxRsApplicationDescriptor;
 import com.liferay.object.system.SystemObjectDefinitionManager;
 import com.liferay.object.system.SystemObjectDefinitionManagerRegistry;
 import com.liferay.object.test.util.ObjectDefinitionTestUtil;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
@@ -32,6 +38,7 @@ import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
@@ -42,15 +49,24 @@ import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
+import com.liferay.portal.odata.entity.EntityField;
+import com.liferay.portal.odata.entity.EntityModel;
 import com.liferay.portal.test.rule.FeatureFlags;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.util.PropsValues;
+import com.liferay.portal.vulcan.resource.EntityModelResource;
 import com.liferay.portal.vulcan.util.LocalizedMapUtil;
+
+import java.lang.reflect.Method;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -58,6 +74,9 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
@@ -189,6 +208,73 @@ public class OpenAPIResourceTest {
 		_assertOpenAPI(
 			"expected_openapi_categorization_disabled.json",
 			categorizationDisabledObjectDefinition);
+	}
+
+	@Test
+	public void testGetOpenAPIFilterableFields() throws Exception {
+		ObjectDefinition objectDefinition =
+			ObjectDefinitionLocalServiceUtil.getObjectDefinition(
+				_objectDefinition.getObjectDefinitionId());
+
+		ObjectDefinition relatedObjectDefinition =
+			ObjectDefinitionTestUtil.publishObjectDefinition(
+				"Object2",
+				Collections.singletonList(
+					ObjectFieldUtil.createObjectField(
+						ObjectFieldConstants.BUSINESS_TYPE_TEXT,
+						ObjectFieldConstants.DB_TYPE_STRING, true, true, null,
+						"field", "field", false)),
+				ObjectDefinitionConstants.SCOPE_COMPANY);
+
+		ObjectRelationshipLocalServiceUtil.addObjectRelationship(
+			null, TestPropsValues.getUserId(),
+			_objectDefinition.getObjectDefinitionId(),
+			relatedObjectDefinition.getObjectDefinitionId(), 0,
+			ObjectRelationshipConstants.DELETION_TYPE_PREVENT,
+			LocalizedMapUtil.getLocalizedMap("relationship1"), "relationship1",
+			false, ObjectRelationshipConstants.TYPE_MANY_TO_MANY, null);
+		ObjectRelationshipLocalServiceUtil.addObjectRelationship(
+			null, TestPropsValues.getUserId(),
+			_objectDefinition.getObjectDefinitionId(),
+			relatedObjectDefinition.getObjectDefinitionId(), 0,
+			ObjectRelationshipConstants.DELETION_TYPE_PREVENT,
+			LocalizedMapUtil.getLocalizedMap("relationship2"), "relationship2",
+			false, ObjectRelationshipConstants.TYPE_ONE_TO_MANY, null);
+
+		Map<String, EntityField> entityFieldsMap =
+			_getObjectDefinitionEntityFieldsMap(objectDefinition);
+
+		Set<String> expectedFilterableFields = entityFieldsMap.keySet();
+
+		JSONObject openAPIJSONObject = HTTPTestUtil.invokeToJSONObject(
+			null, objectDefinition.getRESTContextPath() + "/openapi.json",
+			Http.Method.GET);
+
+		Set<String> actualFilterableFields = _getFilterableFieldsFromOpenAPI(
+			openAPIJSONObject);
+
+		Assert.assertEquals(
+			"Mismatch between entity model filterable fields and OpenAPI " +
+				"x-filterable fields",
+			expectedFilterableFields, actualFilterableFields);
+
+		entityFieldsMap = _getObjectDefinitionEntityFieldsMap(
+			relatedObjectDefinition);
+
+		expectedFilterableFields = entityFieldsMap.keySet();
+
+		openAPIJSONObject = HTTPTestUtil.invokeToJSONObject(
+			null,
+			relatedObjectDefinition.getRESTContextPath() + "/openapi.json",
+			Http.Method.GET);
+
+		actualFilterableFields = _getFilterableFieldsFromOpenAPI(
+			openAPIJSONObject);
+
+		Assert.assertEquals(
+			"Mismatch between entity model filterable fields and OpenAPI " +
+				"x-filterable fields",
+			expectedFilterableFields, actualFilterableFields);
 	}
 
 	@Test
@@ -350,6 +436,103 @@ public class OpenAPIResourceTest {
 				Http.Method.GET
 			).toString(),
 			JSONCompareMode.STRICT);
+	}
+
+	private Set<String> _getFilterableFieldsFromOpenAPI(JSONObject jsonObject) {
+		Set<String> filterableFields = new HashSet<>();
+
+		JSONObject componentsJSONObject = jsonObject.getJSONObject(
+			"components");
+
+		if (componentsJSONObject == null) {
+			return filterableFields;
+		}
+
+		JSONObject schemasJSONObject = componentsJSONObject.getJSONObject(
+			"schemas");
+
+		if (schemasJSONObject == null) {
+			return filterableFields;
+		}
+
+		for (String schemaName : schemasJSONObject.keySet()) {
+			JSONObject schemaJSONObject = schemasJSONObject.getJSONObject(
+				schemaName);
+
+			JSONObject propertiesJSONObject = schemaJSONObject.getJSONObject(
+				"properties");
+
+			if (propertiesJSONObject == null) {
+				continue;
+			}
+
+			for (String propertyName : propertiesJSONObject.keySet()) {
+				JSONObject propertyJSONObject =
+					propertiesJSONObject.getJSONObject(propertyName);
+
+				if ((propertyJSONObject != null) &&
+					propertyJSONObject.has("x-filterable")) {
+
+					boolean filterable = propertyJSONObject.getBoolean(
+						"x-filterable");
+
+					if (filterable) {
+						if (schemaName.equals("UserGroupBrief") &&
+							propertyName.equals("id")) {
+
+							propertyName = Field.USER_ID;
+						}
+						else if (schemaName.equals("Creator") &&
+								 propertyName.equals("id")) {
+
+							propertyName = "creatorId";
+						}
+
+						filterableFields.add(propertyName);
+					}
+				}
+			}
+		}
+
+		return filterableFields;
+	}
+
+	private Map<String, EntityField> _getObjectDefinitionEntityFieldsMap(
+			ObjectDefinition objectDefinition)
+		throws Exception {
+
+		Map<String, EntityField> objectEntityFieldsMap = null;
+
+		Bundle bundle = FrameworkUtil.getBundle(
+			ObjectEntryEntityModelTest.class);
+
+		ServiceTrackerMap<String, ObjectEntryResource> serviceTrackerMap =
+			ServiceTrackerMapFactory.openSingleValueMap(
+				bundle.getBundleContext(), ObjectEntryResource.class,
+				"entity.class.name");
+
+		ObjectEntryResource objectEntryResource = serviceTrackerMap.getService(
+			StringBundler.concat(
+				ObjectEntry.class.getName(), StringPool.POUND,
+				StringUtil.toLowerCase(objectDefinition.getName())));
+
+		if (objectEntryResource instanceof EntityModelResource) {
+			Class<?> clazz = objectEntryResource.getClass();
+
+			Method method = clazz.getMethod(
+				"setObjectDefinition", ObjectDefinition.class);
+
+			method.invoke(objectEntryResource, objectDefinition);
+
+			EntityModelResource entityModelResource =
+				(EntityModelResource)objectEntryResource;
+
+			EntityModel entityModel = entityModelResource.getEntityModel(null);
+
+			objectEntityFieldsMap = entityModel.getEntityFieldsMap();
+		}
+
+		return objectEntityFieldsMap;
 	}
 
 	@Inject
