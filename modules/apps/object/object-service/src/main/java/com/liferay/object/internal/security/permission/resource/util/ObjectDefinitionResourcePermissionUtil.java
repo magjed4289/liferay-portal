109990cd2af64c5f295f0d8f5f37ef858d5f564c
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2025-05-06 18:28:13 +0200

    LPD-45633 [TEST]

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/security/permission/resource/util/ObjectDefinitionResourcePermissionUtil.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/security/permission/resource/util/ObjectDefinitionResourcePermissionUtil.java
@@ -18,6 +18,8 @@ import com.liferay.object.tree.ObjectDefinitionTreeFactory;
 import com.liferay.object.tree.Tree;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.ResourceActions;
@@ -68,6 +70,29 @@ public class ObjectDefinitionResourcePermissionUtil {
 			objectDefinition.getCompanyId(), objectDefinition.getPortletId());
 
 		if (portlet != null) {
+			List<ObjectDefinition> allRootObjectDefinitions =
+				objectDefinitionPersistence.findAll();
+
+			for (ObjectDefinition other : allRootObjectDefinitions) {
+				if (other.isPortlet() && !other.isRootDescendantNode() &&
+					!Objects.equals(
+						other.getObjectDefinitionId(),
+						objectDefinition.getObjectDefinitionId()) &&
+					Objects.equals(
+						other.getPortletId(),
+						objectDefinition.getPortletId())) {
+
+					System.out.println("Skipping portlet resource registration for objectDefinitionId " +
+									   objectDefinition.getObjectDefinitionId() +
+									   " because portletId " +
+									   objectDefinition.getPortletId() +
+									   " is already in use by objectDefinitionId " +
+									   other.getObjectDefinitionId()));
+
+					return;
+				}
+			}
+
 			resourceActions.populatePortletResource(
 				portlet,
 				ObjectDefinitionResourcePermissionUtil.class.getClassLoader(),
