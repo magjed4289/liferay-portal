934f1ae21e19e39dc7908c60d426aa7a60130f85
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2025-07-29 12:09:26 +0200

    LPD-61326 Simplify. With two virtual instances, there's no need to restart the bundle

--- modules/apps/headless/headless-builder/headless-builder-test/src/testIntegration/java/com/liferay/headless/builder/object/deployer/test/APIPropertyObjectDefinitionDeployerTest.java
+++ modules/apps/headless/headless-builder/headless-builder-test/src/testIntegration/java/com/liferay/headless/builder/object/deployer/test/APIPropertyObjectDefinitionDeployerTest.java
@@ -6,22 +6,18 @@
 package com.liferay.headless.builder.object.deployer.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.batch.engine.unit.BatchEngineUnitProcessor;
-import com.liferay.batch.engine.unit.BatchEngineUnitReader;
-import com.liferay.headless.builder.test.util.HeadlessBuilderUtil;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
 import com.liferay.portal.kernel.dao.db.DBType;
 import com.liferay.portal.kernel.db.partition.DBPartition;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
-import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.test.TestInfo;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.AssumeTestRule;
 import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.util.HTTPTestUtil;
 import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.test.rule.FeatureFlag;
-import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.util.PropsValues;
 
@@ -56,7 +52,8 @@ public class APIPropertyObjectDefinitionDeployerTest {
 	}
 
 	@Test
-	public void testAPIBuilderObjectDefinitionsCreatedAndPublished()
+	@TestInfo("LPD-61326")
+	public void testAPIBuilderObjectDefinitionsCreatedAndPublishedWithDBPartitioningEnabled()
 		throws Exception {
 
 		HTTPTestUtil.invokeToJSONObject(
@@ -69,9 +66,6 @@ public class APIPropertyObjectDefinitionDeployerTest {
 			).toString(),
 			"headless-portal-instances/v1.0/portal-instances",
 			Http.Method.POST);
-
-		HeadlessBuilderUtil.deploy(
-			_batchEngineUnitProcessor, _batchEngineUnitReader);
 
 		HTTPTestUtil.customize(
 		).withBaseURL(
@@ -91,15 +85,36 @@ public class APIPropertyObjectDefinitionDeployerTest {
 					"true", apiPropertyJSONObject.getString("active"));
 			}
 		);
+
+		HTTPTestUtil.invokeToJSONObject(
+			JSONUtil.put(
+				"domain", "example.com"
+			).put(
+				"portalInstanceId", "example.com"
+			).put(
+				"virtualHost", "www.example.com"
+			).toString(),
+			"headless-portal-instances/v1.0/portal-instances",
+			Http.Method.POST);
+
+		HTTPTestUtil.customize(
+		).withBaseURL(
+			"http://www.example.com:8080"
+		).withCredentials(
+			"test@example.com", PropsValues.DEFAULT_ADMIN_PASSWORD
+		).apply(
+			() -> {
+				JSONObject apiPropertyJSONObject =
+					HTTPTestUtil.invokeToJSONObject(
+						null,
+						"object-admin/v1.0/object-definitions" +
+							"/by-external-reference-code/L_API_PROPERTY",
+						Http.Method.GET);
+
+				Assert.assertEquals(
+					"true", apiPropertyJSONObject.getString("active"));
+			}
+		);
 	}
-
-	@Inject
-	private BatchEngineUnitProcessor _batchEngineUnitProcessor;
-
-	@Inject
-	private BatchEngineUnitReader _batchEngineUnitReader;
-
-	@Inject
-	private CompanyLocalService _companyLocalService;
 
 }
