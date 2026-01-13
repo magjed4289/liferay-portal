0efce6cae3f5e7d7b026e4034f810ee0c08545f2
Magdalena Jedraszak <magdalena.jedraszak@liferay.com>
2026-01-13 10:40:51 +0100

    Revert "LPD-67533 Make sure the checkbox is selected"
    This reverts commit a83c78d6fb15f46f8de05224497d47cae13d131e.

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/data/handler/BatchEnginePortletDataHandler.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/data/handler/BatchEnginePortletDataHandler.java
@@ -241,7 +241,6 @@ public class BatchEnginePortletDataHandler extends BasePortletDataHandler {
 			setDataLevel(DataLevel.SITE);
 		}
 
-		setPublishToLiveByDefault(true);
 		_updateDeletionSystemEventStagedModelTypes();
 		_updateExportPortletDataHandlerControls();
 	}
